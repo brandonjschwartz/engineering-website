2c834d8450abcca7bff4fb4393f48e30672d66b1
Oren <ogolan@attinteractive.com>
2012-09-24 14:02:18 -0700

    start script

--- updater/server.js
+++ updater/server.js
@@ -3,29 +3,30 @@ var spawn = require('child_process').spawn;
 
 http.createServer(function(req, res) {
   if (req.method === 'POST' && req.url === '/update') {
-    console.log('git pull');
-
-    var git = spawn('git', ['pull', 'origin', 'master']);
-
-    git.stdout.on('data', function (data) {
-      console.log('git stdout: ' + data);
-    });
-
-    git.stderr.on('data', function (data) {
-      console.log('git stderr: ' + data);
-    });
-
-    git.on('exit', function (code) {
-      console.log('git child process exited with code ' + code);
-      restartDocpad();
-    });
+    gitPull();
   }
-  
+
   res.end();
 }).listen(3000);
 
 console.log('listening on http://localhost:3000');
 
+function gitPull() {
+  var git = spawn('git', ['pull', 'origin', 'master']);
+
+  git.stdout.on('data', function (data) {
+    console.log('git stdout: ' + data);
+  });
+
+  git.stderr.on('data', function (data) {
+    console.log('git stderr: ' + data);
+  });
+
+  git.on('exit', function (code) {
+    console.log('git child process exited with code ' + code);
+    restartDocpad();
+  });
+};
 
 function restartDocpad() {
   var forever = spawn('forever', ['restart', '0']);
@@ -41,6 +42,5 @@ function restartDocpad() {
   forever.on('exit', function (code) {
     console.log('forever child process exited with code ' + code);
     forever = spawn('forever', ['restart', '0']);
-
   });
 };
