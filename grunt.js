f007064baf408353bf37f6b3eb93804ad20a333d
Oren <ogolan@attinteractive.com>
2012-10-14 19:24:39 -0700

    add lint to grunt

--- grunt.js
+++ grunt.js
@@ -31,16 +31,35 @@ module.exports = function(grunt) {
           'public/js/jquery.tweet.js',
           'public/js/jquery.ghRepo.js',
           'public/js/prettify.js',
-          'public/js/ui.js'
+          'public/js/ui.js',
+          'public/js/app.js'
         ],
         dest: 'public/js/site.js'
+      }
+    },
+    lint: {
+      files: ['public/js/app.js', 'grunt.js']
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        // undef: true
+        boss: true,
+        eqnull: true
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib');
   grunt.loadNpmTasks('grunt-css');
+  grunt.loadNpmTasks('grunt-browserify');
 
-  grunt.registerTask('default', 'stylus cssmin concat min');
+  grunt.registerTask('default', 'lint stylus cssmin concat min');
 
 };
